b4415740e6f918b072e15a13d99bd50b8a6fc45e
onekaking <thient91@gmail.com>
2013-03-25 08:56:31 +0700

    Create util.js

---
+++ util.js
@@ -0,0 +1,161 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: user
+ * Date: 3/21/13
+ * Time: 6:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var jj = jj || (function() {
+    var ajax = {
+        xmlHttp : null,
+        createXMLHTTP : function() {
+            if (this.xmlHttp) {
+                if (window.XMLHttpRequest)
+                {// code for IE7+, Firefox, Chrome, Opera, Safari
+                    this.xmlHttp=new XMLHttpRequest();
+                } else {// code for IE6, IE5
+                    this.xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+                }
+            }
+        },
+        sendAjax :function(e) {
+            if (e || typeof e != 'object') {
+                console.log("Error : Input object config");
+                return;
+            }
+            this.createXMLHTTP();
+
+            this.xmlHttp.onreadystatechange=function() {
+                if (this.xmlHttp.readyState==4 && this.xmlHttp.status==200) {
+                    e.success(this.xmlHttp.responseText);
+                }
+            }
+
+            this.xmlHttp.open(e.type || "GET", e.url + (e.type == "POST" ? "" : (e.data ? "?" + e.data : "" )) ,true);
+            this.xmlHttp.setRequestHeader("Content-type", e["Content-Type"] ? e["Content-Type"] : "application/x-www-form-urlencoded");
+            this.xmlHttp.send(e.type == "GET" ? null : (e.data ? e.data : null));
+        }
+    };
+
+    var postBack  = {
+
+    };
+
+    var el = {
+        attr : function() {
+            console.log(document.getAnonymousElementByAttribute(document.body,'data-popup','true'));
+        },
+        get : function(el) {
+            var obj;
+            if (el.indexOf('#') != -1) {
+                el = el.replace('#','');
+                obj = document.getElementById(el);
+            } else if (el.indexOf('.') != -1) {
+                el = el.replace('.','');
+                obj = document.getElementsByClassName(el);
+            } else {
+                obj = document.getElementsByTagName(el)[0];
+            }
+            return obj;
+        }
+    };
+
+    var event = {
+        click : function(e,cb) {
+            var obj;
+            if (typeof el != "object") {
+                obj = el.get(e);
+            }
+            if (obj.length) {
+                for(var i =0;i<obj.length ; i++) {
+                    this.addEvent('click',obj[i],cb);
+                }
+            } else {
+                this.addEvent('click',obj);
+            }
+        },
+        addEvent : function(event,target,cb) {
+            if (target.addEventListener) {
+                target.addEventListener(event, cb, false);
+            } else if (target.attachEvent) {
+                target.attachEvent("on" + event, cb);
+            }
+        }
+    }
+
+    var xml = {
+        parse : function(text) {
+            if (window.DOMParser)
+            {
+                var parser=new DOMParser();
+                var xmlDoc= parser.parseFromString(text,"text/xml");
+                //console.log(typeof xmlDoc);
+                return xmlDoc;
+            }
+            else // Internet Explorer
+            {
+                var xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+                xmlDoc.async=false;
+                xmlDoc.loadXML(text);
+                return xmlDoc;
+            }
+        },
+        show : function() {
+            console.log(typeof this);
+        }
+    }
+
+    var ui = {
+        button : function(conf) {
+            var e = document.createElement('div');
+            e.innerHTML = conf.v;
+            e.classList.add('btn');
+            if (conf.class) {
+                e.classList.add(conf.class);
+            }
+            var obj = conf.e;
+            if (typeof conf.e != "object") {
+                obj = el.get(conf.e);
+            }
+            obj.appendChild(e);
+            return e;
+        },
+        text : function(conf) {
+
+        },
+        comboBox : function(conf) {
+
+        }
+    }
+
+    var slide = {
+        get : function(){
+
+        }
+    }
+
+    var support = {
+        add : function(config) {
+
+        },
+        remove : function(el) {
+
+        },
+        get : function(config){
+            ajax.sendAjax({
+                url : '',
+                success : function(data) {
+
+                }
+            });
+        }
+    }
+
+    return {
+        sendAjax : ajax.sendAjax,
+        click : event.click,
+        parseXML : xml.parse,
+        ui : ui
+    };
+}());
